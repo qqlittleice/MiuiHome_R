64c993bb334362c630a853a4fde0cc7d073c0ccb
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-07-05 15:58:04 +0800

    Fix AnimDurationRatio Feature

--- app/src/main/java/com/yuk/miuiHomeR/hook/AnimDurationRatio.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/AnimDurationRatio.kt
@@ -1,17 +1,19 @@
 package com.yuk.miuiHomeR.hook
 
 import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookReturnConstant
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.yuk.miuiHomeR.mPrefsMap
 
 object AnimDurationRatio : BaseHook() {
     override fun init() {
 
-        val value = mPrefsMap.getInt("prefs_key_home_anim_ratio", 100)
-        if (value == 100) return
-        findMethod("com.miui.home.recents.TransitionAnimDurationHelper") {
-            name == "getAnimDurationRatio"
-        }.hookReturnConstant(value.toFloat() / 100)
+        val value = mPrefsMap.getInt("home_anim_ratio", 100).toFloat() / 100f
+        if (value == 1f) return
+        findMethod("com.miui.home.recents.util.RectFSpringAnim") {
+            name == "getModifyResponse"
+        }.hookBefore {
+            it.result = it.args[0] as Float * value
+        }
     }
 
 }
