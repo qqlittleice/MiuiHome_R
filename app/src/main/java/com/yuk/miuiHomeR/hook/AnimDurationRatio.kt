bd500b793a76d41476662e4f8056444955445ea8
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-07-15 17:17:19 +0800

    修复后台动画速度跟随
    * 尝试修复修改后进入后台渐进式加快或减慢的问题

--- app/src/main/java/com/yuk/miuiHomeR/hook/AnimDurationRatio.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/AnimDurationRatio.kt
@@ -1,8 +1,8 @@
 package com.yuk.miuiHomeR.hook
 
-import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import com.github.kyuubiran.ezxhelper.utils.*
 import com.yuk.miuiHomeR.mPrefsMap
+import kotlin.math.pow
 
 object AnimDurationRatio : BaseHook() {
     override fun init() {
@@ -13,6 +13,26 @@ object AnimDurationRatio : BaseHook() {
             name == "getModifyResponse"
         }.hookBefore {
             it.result = it.args[0] as Float * value
+        }
+        findMethod("com.miui.home.recents.util.RectFSpringAnim") {
+            name == "reset" && parameterCount == 6
+        }.hookAfter {
+            val x = it.thisObject.getObject("DEFAULT_CENTER_X_STIFFNESS") as Float
+            val y = it.thisObject.getObject("DEFAULT_CENTER_Y_STIFFNESS") as Float
+            val width = it.thisObject.getObject("DEFAULT_WIDTH_STIFFNESS") as Float
+            val ratio = it.thisObject.getObject("DEFAULT_RATIO_STIFFNESS") as Float
+            val radius = it.thisObject.getObject("DEFAULT_RADIUS_STIFFNESS") as Float
+            val alpha = it.thisObject.getObject("DEFAULT_ALPHA_STIFFNESS") as Float
+
+            val pow = value.toDouble().pow(2.0)
+
+            it.thisObject.putObject("mCenterXStiffness", (x / pow).toFloat())
+            it.thisObject.putObject("mCenterYStiffness", (y / pow).toFloat())
+            it.thisObject.putObject("mWidthStiffness", (width / pow).toFloat())
+            it.thisObject.putObject("mRatioStiffness", (ratio / pow).toFloat())
+            it.thisObject.putObject("mRadiusStiffness", (radius / pow).toFloat())
+            it.thisObject.putObject("mAlphaStiffness", (alpha / pow).toFloat())
+
         }
     }
 
