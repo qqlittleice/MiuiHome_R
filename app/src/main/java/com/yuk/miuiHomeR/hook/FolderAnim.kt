1d69fccb6539da90e998d1d944e8d4606eba2928
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-09-10 11:23:36 +0800

    查找文件夹回弹动画所在类

--- app/src/main/java/com/yuk/miuiHomeR/hook/FolderAnim.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/FolderAnim.kt
@@ -4,39 +4,39 @@ import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookMethod
 import com.yuk.miuiHomeR.mPrefsMap
 import com.yuk.miuiHomeR.utils.ktx.findClass
+import com.yuk.miuiHomeR.utils.ktx.findClassOrNull
 import com.yuk.miuiHomeR.utils.ktx.hookBeforeMethod
 import de.robv.android.xposed.XC_MethodHook
-
 
 object FolderAnim : BaseHook() {
     override fun init() {
 
         if (!mPrefsMap.getBoolean("home_folder_anim")) return
         var hook: XC_MethodHook.Unhook? = null
-        try {
-            findMethod("com.miui.home.launcher.Launcher$50") {
-                name == "run"
-            }
-        } catch (e: Exception) {
-            findMethod("com.miui.home.launcher.Launcher$49") {
-                name == "run"
-            }
-        } catch (e: Exception) {
-            findMethod("com.miui.home.launcher.Launcher$47") {
-                name == "run"
-            }
-        }.hookMethod {
-            before {
-                val mSpringAnimator = "com.miui.home.launcher.animate.SpringAnimator".findClass()
-                hook = mSpringAnimator.hookBeforeMethod(
-                    "setDampingResponse", Float::class.javaPrimitiveType, Float::class.javaPrimitiveType
-                ) {
-                    it.args[0] = 0.5f
-                    it.args[1] = 0.5f
+        for (i in 47..60) {
+            val launcherClass = "com.miui.home.launcher.Launcher$$i".findClassOrNull()
+            if (launcherClass != null) {
+                for (field in launcherClass.declaredFields) {
+                    if (field.name == "val\$folderInfo") {
+                        findMethod(launcherClass) {
+                            name == "run"
+                        }.hookMethod {
+                            before {
+                                val mSpringAnimator = "com.miui.home.launcher.animate.SpringAnimator".findClass()
+                                hook = mSpringAnimator.hookBeforeMethod(
+                                    "setDampingResponse", Float::class.javaPrimitiveType, Float::class.javaPrimitiveType
+                                ) {
+                                    it.args[0] = 0.5f
+                                    it.args[1] = 0.5f
+                                }
+                            }
+                            after {
+                                hook?.unhook()
+                            }
+                        }
+                        break
+                    }
                 }
-            }
-            after {
-                hook?.unhook()
             }
         }
     }
