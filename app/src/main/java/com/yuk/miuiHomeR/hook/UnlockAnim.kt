75af698bba69a517b8ceea70fb7041350fd8abd1
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-09-10 13:29:34 +0800

    更换优化解锁动画实现方式

--- app/src/main/java/com/yuk/miuiHomeR/hook/UnlockAnim.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/UnlockAnim.kt
@@ -1,19 +1,18 @@
 package com.yuk.miuiHomeR.hook
 
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.yuk.miuiHomeR.mPrefsMap
-import com.yuk.miuiHomeR.utils.ktx.findClass
-import com.yuk.miuiHomeR.utils.ktx.hookBeforeMethod
 
 object UnlockAnim : BaseHook() {
     override fun init() {
 
         if (!mPrefsMap.getBoolean("home_unlock_anim")) return
-        val mSpringAnimator = "com.miui.home.launcher.animate.SpringAnimator".findClass()
-        mSpringAnimator.hookBeforeMethod(
-            "setDampingResponse", Float::class.javaPrimitiveType, Float::class.javaPrimitiveType
-        ) {
-            it.args[0] = 0.5f
-            it.args[1] = 0.5f
+        findMethod("com.miui.home.launcher.compat.UserPresentAnimationCompatV12Phone") {
+            name == "getSpringAnimator" && parameterCount == 6
+        }.hookBefore {
+            it.args[4] = 0.5f
+            it.args[5] = 0.5f
         }
     }
 
