eaa976b1f600ad1b771c33752245f89ad506b813
xiao_wine <xiaowine0@gmail.com>
2022-09-12 23:01:17 +0800

    添加配置 备份与恢复

--- app/src/main/java/com/yuk/miuiHomeR/ui/MainActivity.kt
+++ app/src/main/java/com/yuk/miuiHomeR/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yuk.miuiHomeR.ui
 
 import android.content.ComponentName
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.content.pm.PackageManager
@@ -16,8 +17,10 @@ import com.yuk.miuiHomeR.R
 import com.yuk.miuiHomeR.provider.SharedPrefsProvider
 import com.yuk.miuiHomeR.ui.base.BaseAppCompatActivity
 import com.yuk.miuiHomeR.ui.base.SubFragment
+import com.yuk.miuiHomeR.utils.BackupUtils
 import com.yuk.miuiHomeR.utils.Helpers
 import com.yuk.miuiHomeR.utils.PrefsUtils
+
 
 class MainActivity : BaseAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,16 +41,10 @@ class MainActivity : BaseAppCompatActivity() {
                 is Int -> path = "integer/"
                 is Boolean -> path = "boolean/"
             }
-            contentResolver.notifyChange(
-                Uri.parse("content://" + SharedPrefsProvider.AUTHORITY + "/" + path + s), null
-            )
-            if (path != "") contentResolver.notifyChange(
-                Uri.parse("content://" + SharedPrefsProvider.AUTHORITY + "/pref/" + path + s), null
-            )
+            contentResolver.notifyChange(Uri.parse("content://" + SharedPrefsProvider.AUTHORITY + "/" + path + s), null)
+            if (path != "") contentResolver.notifyChange(Uri.parse("content://" + SharedPrefsProvider.AUTHORITY + "/pref/" + path + s), null)
         }
-        PrefsUtils.mSharedPreferences.registerOnSharedPreferenceChangeListener(
-            mPreferenceChangeListener
-        )
+        PrefsUtils.mSharedPreferences.registerOnSharedPreferenceChangeListener(mPreferenceChangeListener)
         Helpers.fixPermissionsAsync(applicationContext)
         try {
             val fileObserver: FileObserver = object : FileObserver(PrefsUtils.getSharedPrefsPath(), CLOSE_WRITE) {
@@ -75,50 +72,64 @@ class MainActivity : BaseAppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
-        val state = packageManager.getComponentEnabledSetting(
-            ComponentName(
-                this, this.javaClass.name + "Alias"
-            )
-        )
+        val state = packageManager.getComponentEnabledSetting(ComponentName(this, this.javaClass.name + "Alias"))
         menu.findItem(R.id.icon).title = when (state) {
             PackageManager.COMPONENT_ENABLED_STATE_DISABLED -> {
                 getString(R.string.show_icon)
             }
+
             PackageManager.COMPONENT_ENABLED_STATE_ENABLED -> {
                 getString(R.string.hide_icon)
             }
+
             else -> getString(R.string.hide_icon)
         }
         return super.onCreateOptionsMenu(menu)
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.reset -> {
-
             }
+
             R.id.backup -> {
-
+                BackupUtils.backup(this, PrefsUtils.mSharedPreferences)
             }
+
             R.id.restore -> {
-
+                BackupUtils.recovery(this, PrefsUtils.mSharedPreferences)
             }
+
             R.id.icon -> {
-                packageManager.setComponentEnabledSetting(
-                    ComponentName(this, this.javaClass.name + "Alias"),
-                    if (item.title == getString(R.string.hide_icon)) PackageManager.COMPONENT_ENABLED_STATE_DISABLED
-                    else PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                    PackageManager.DONT_KILL_APP
-                )
+                packageManager.setComponentEnabledSetting(ComponentName(this, this.javaClass.name + "Alias"), if (item.title == getString(R.string.hide_icon)) PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                else PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
                 Shell.cmd("am force-stop com.yuk.miuiHomeR").exec()
             }
+
             R.id.about -> {
                 startActivity(this, AboutActivity::class.java)
             }
+
             R.id.reboot_home -> {
                 Shell.cmd("am force-stop com.miui.home", "am force-stop com.yuk.miuiHomeR").exec()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null) return
+        when (requestCode) {
+            BackupUtils.CREATE_DOCUMENT_CODE -> {
+                BackupUtils.handleCreateDocument(this, data.data)
+            }
+
+            BackupUtils.OPEN_DOCUMENT_CODE -> {
+                BackupUtils.handleReadDocument(this, data.data)
+            }
+        }
     }
 }
