b5fdd1c6d53bd1574cfb775860a635dfa329a639
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-09-17 13:19:54 +0800

    优化允许小部件移到到负一屏
    * 取消 1x1 与 1x3 限制

--- app/src/main/java/com/yuk/miuiHomeR/hook/AllowMoveAllWidgetToMinus.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/AllowMoveAllWidgetToMinus.kt
@@ -1,7 +1,11 @@
 package com.yuk.miuiHomeR.hook
 
-import com.github.kyuubiran.ezxhelper.utils.*
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.getObjectOrNull
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import com.github.kyuubiran.ezxhelper.utils.invokeMethodAuto
 import com.yuk.miuiHomeR.mPrefsMap
+import com.yuk.miuiHomeR.utils.ktx.callMethod
 
 object AllowMoveAllWidgetToMinus : BaseHook() {
     override fun init() {
@@ -9,10 +13,11 @@ object AllowMoveAllWidgetToMinus : BaseHook() {
         if (!mPrefsMap.getBoolean("home_widget_to_minus")) return
         findMethod("com.miui.home.launcher.Workspace") {
             name == "canDragToPa"
-        }.hookAfter {
-            val currentDragObject = it.thisObject.getObjectOrNull("mDragController")?.invokeMethodAuto("getCurrentDragObject")
-            val dragInfo = currentDragObject?.invokeMethodAuto("getDragInfo")
-            dragInfo?.putObject("isMIUIWidget", true)
+        }.hookBefore {
+            val launcherCallbacks = it.thisObject.getObjectOrNull("mLauncher")?.invokeMethodAuto("getLauncherCallbacks")
+            val isDraggingFromAssistant = it.thisObject.getObjectOrNull("mDragController")?.callMethod("isDraggingFromAssistant") as Boolean
+            val isDraggingToAssistant = it.thisObject.getObjectOrNull("mDragController")?.callMethod("isDraggingToAssistant") as Boolean
+            it.result = launcherCallbacks != null && !isDraggingFromAssistant && !isDraggingToAssistant
         }
 
     }
