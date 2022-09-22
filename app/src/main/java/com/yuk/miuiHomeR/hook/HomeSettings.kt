9349428226ee66f000459a441b030b8c4a87351a
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-09-22 23:12:56 +0800

    移除 FLAG_ACTIVITY_NEW_TASK

--- app/src/main/java/com/yuk/miuiHomeR/hook/HomeSettings.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/HomeSettings.kt
@@ -6,14 +6,12 @@ import android.view.View
 import com.github.kyuubiran.ezxhelper.init.InitFields.appContext
 import com.github.kyuubiran.ezxhelper.init.InitFields.moduleRes
 import com.yuk.miuiHomeR.R
-import com.yuk.miuiHomeR.ui.MainActivity
 import com.yuk.miuiHomeR.utils.ktx.callMethod
 import com.yuk.miuiHomeR.utils.ktx.findClass
 import com.yuk.miuiHomeR.utils.ktx.getObjectField
 import com.yuk.miuiHomeR.utils.ktx.hookAfterAllMethods
 import com.yuk.miuiHomeR.utils.ktx.setObjectField
 import de.robv.android.xposed.XposedHelpers
-
 
 object HomeSettings : BaseHook() {
 
@@ -33,7 +31,6 @@ object HomeSettings : BaseHook() {
                     override fun onClick(v: View) {
                         val intent = Intent()
                         intent.component = ComponentName("com.yuk.miuiHomeR", "com.yuk.miuiHomeR.ui.MainActivity")
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                         v.context.startActivity(intent)
                     }
                 })
