15b3b4d9198ad262d930a9b7539c3c2838ac1d89
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-12-13 17:47:33 +0800

    加强 中等大小图标圆角跟随

--- app/src/main/java/com/yuk/miuiHomeR/hook/BigIconCorner.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/BigIconCorner.kt
@@ -3,6 +3,7 @@ package com.yuk.miuiHomeR.hook
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.yuk.miuiHomeR.mPrefsMap
+import com.yuk.miuiHomeR.utils.ktx.getObjectField
 
 object BigIconCorner : BaseHook() {
     override fun init() {
@@ -14,5 +15,26 @@ object BigIconCorner : BaseHook() {
             it.args[0] = 2
             it.args[1] = 2
         }
+
+        findMethod(
+            "com.miui.home.launcher.maml.MaMlHostView"
+        ) {
+            name == "getCornerRadius"
+        }.hookBefore {
+            it.result = it.thisObject.getObjectField("mEnforcedCornerRadius") as Float
+        }
+
+        findMethod("com.miui.home.launcher.maml.MaMlHostView") {
+            name == "computeRoundedCornerRadius" && parameterCount == 1
+        }.hookBefore {
+            it.result = it.thisObject.getObjectField("mEnforcedCornerRadius") as Float
+        }
+
+        findMethod("com.miui.home.launcher.LauncherAppWidgetHostView") {
+            name == "computeRoundedCornerRadius" && parameterCount == 1
+        }.hookBefore {
+            it.result = it.thisObject.getObjectField("mEnforcedCornerRadius") as Float
+        }
+
     }
 }
