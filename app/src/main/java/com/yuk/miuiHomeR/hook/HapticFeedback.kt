158ee544b50a4cc91350b44afd52f8c6bd0dca4f
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-12-15 20:48:19 +0800

    替换为 replaceMethod

--- app/src/main/java/com/yuk/miuiHomeR/hook/HapticFeedback.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/HapticFeedback.kt
@@ -1,26 +1,26 @@
 package com.yuk.miuiHomeR.hook
 
 import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.yuk.miuiHomeR.mPrefsMap
 import com.yuk.miuiHomeR.utils.ktx.callMethod
 import com.yuk.miuiHomeR.utils.ktx.getObjectField
+import com.yuk.miuiHomeR.utils.ktx.replaceMethod
 
 object HapticFeedback : BaseHook() {
     override fun init() {
 
         if (!mPrefsMap.getBoolean("haptic_feedback")) return
-        val value = mPrefsMap.getInt("haptic_Back_HandUp",2)
-        val value1 = mPrefsMap.getInt("haptic_Back_ReadyBack",2)
+        val value = mPrefsMap.getInt("haptic_Back_HandUp", 2)
+        val value1 = mPrefsMap.getInt("haptic_Back_ReadyBack", 2)
         findMethod("com.miui.home.launcher.common.HapticFeedbackCompatLinear") {
             name == "lambda\$performGestureBackHandUp\$8" && parameterCount == 1
-        }.hookAfter {
+        }.replaceMethod {
             it.args[0].getObjectField("mHapticHelper")?.callMethod("performExtHapticFeedback", value)
         }
 
         findMethod("com.miui.home.launcher.common.HapticFeedbackCompatLinear") {
             name == "lambda\$performGestureReadyBack\$7" && parameterCount == 1
-        }.hookAfter {
+        }.replaceMethod {
             it.args[0].getObjectField("mHapticHelper")?.callMethod("performExtHapticFeedback", value1)
         }
     }
