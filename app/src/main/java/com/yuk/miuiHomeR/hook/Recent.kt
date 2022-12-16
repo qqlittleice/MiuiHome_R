263938cc7579a13238583db835ae848e209fdc9b
YuKongA <70465933+YuKongA@users.noreply.github.com>
2022-12-16 21:14:06 +0800

    移除 “移除「小窗应用」按钮” 功能
    * 官方已经默认去掉了

--- app/src/main/java/com/yuk/miuiHomeR/hook/Recent.kt
+++ app/src/main/java/com/yuk/miuiHomeR/hook/Recent.kt
@@ -14,14 +14,6 @@ import com.yuk.miuiHomeR.utils.ktx.setIntField
 object Recent : BaseHook() {
     override fun init() {
         val recentsContainerClass = "com.miui.home.recents.views.RecentsContainer".findClass()
-        if (mPrefsMap.getBoolean("home_hide_small_window")) {
-            recentsContainerClass.hookAfterMethod(
-                "onFinishInflate"
-            ) {
-                val mTitle = it.thisObject.getObjectField("mTxtSmallWindow") as TextView
-                mTitle.visibility = View.GONE
-            }
-        }
 
         if (mPrefsMap.getBoolean("home_hide_clean_up")) {
             recentsContainerClass.hookAfterMethod(
